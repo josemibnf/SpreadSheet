aafb4c79028a3197387fc84d6491c602389765a4
jal04 <josemi.bnf@outlook.es>
2020-05-13 19:21:02 +0200

    ComplexSpreadSheetTest added

---
+++ tests/ComplexSpreadSheetTest.java
@@ -0,0 +1,33 @@
+import static spreadsheet.SpreadSheet.*;
+//y demas imports ...
+
+import java.beans.Transient;
+
+public class ComplexSpreadSheetTest {
+
+    @BeforeEach
+    public void setUp() {
+        put("c1", mult("a1", "b1")); 
+        put("c2", mult("a2", "b2")); 
+        put("c3", plus("c1", "c2")); 
+
+        put("a1", 10); put("b1", 20);
+        put("a2", 30); put("b2", 40);
+    }
+
+    @Test
+    public void chained_expressions() {
+        assertEquals( new SomeValue(1400), get("c3"));
+    }
+
+    @Test
+    public void chained_propagations() {
+        put("a1", "b1");
+        assertEquals(new SomeValue(1600), get("c3")); 
+    }
+
+    @AfterEach
+    public void tearDown(){
+        clear();
+    }
+}
