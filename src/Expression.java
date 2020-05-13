81eb39b4468ebf2e941f36edd59881bbd372b2fc
jal04 <josemi.bnf@outlook.es>
2020-05-13 17:29:23 +0200

    Expression added

--- src/Expression.java
+++ src/Expression.java
@@ -1,5 +1,11 @@
-
+import java.util.Set;
 
 public interface Expression {
+    public Set<Cell> references;  // Posee el conjunto de celdas que posee
+                                 // la expresion, directa o inderectamente,
+                                // puede ser null.
 
+    public default Set<Cell> getReferences() {  // No se muy bien que es lo de default.
+        return null;
+    }
 }
