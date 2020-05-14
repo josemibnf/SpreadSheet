bd04a2f957eaf66ea3eb00ff21d57c4f64232f17
jal04 <josemi.bnf@outlook.es>
2020-05-14 16:30:24 +0200

    Expression has references method

--- maven/src/main/java/java/Expression.java
+++ maven/src/main/java/java/Expression.java
@@ -1,22 +1,22 @@
 package java;
 
-import java.Cell;
-import java.opt.Operation;
 import java.util.Set;
 import java.value.MaybeValue;
 
 public interface Expression {
     
-    public Set<Cell> references;  // Posee el conjunto de celdas que posee
-                                 // la expresion, directa o inderectamente,
-                                // puede ser null.
+    /**
+     * Posee el conjunto de celdas que posee la expresion,
+     * directa o indirectamente. Puede ser null.
+     * @return
+     */
+    public Set<Cell> references(); 
     
     /**
      * Retorna el valor de la formula.
      * @return
      */
     public MaybeValue evaluate();
-    public Set<Cell> getReferences();
 }
 
 // Las clases deben de ser inmutables.
