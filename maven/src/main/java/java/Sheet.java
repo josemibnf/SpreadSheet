56f3cdbce10e8f830f4d675435b4b3e2c29ff4cb
jal04 <josemi.bnf@outlook.es>
2020-05-14 12:43:20 +0200

    ThisCellDoesNotExist added

--- maven/src/main/java/java/Sheet.java
+++ maven/src/main/java/java/Sheet.java
@@ -1,17 +1,29 @@
 package java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sheet {
     // Esta es la clase Reference ¿??
+
+    private Map<String, Cell> CellSheet = new HashMap<String, Cell>();
 
 	public Sheet(int SIZE) {
         for (int i=0;i<SIZE; i++){
             for (int j=0; j<SIZE; j++){
-                Reference(new Cell());
+                CellSheet.put(, new Cell());
             }
         }
     }
     
-    public static Cell getCell(String ref){
-        return (Cell) null; // Buscamos la celda con esa referencia.
+    public Cell getCell(String ref){
+        if (dontHaveThisCell(ref)){
+            throw ThisCellNotExist;
+        }
+        return CellSheet.get(ref); // Buscamos la celda con esa referencia.
+    }
+
+    private boolean dontHaveThisCell(String ref) {
+        return false;
     }
 }
