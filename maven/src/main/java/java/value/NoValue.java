0fe60f210810177aedeb3b12bedd3eaf9d3e0e28
jal04 <josemi.bnf@outlook.es>
2020-05-14 16:42:31 +0200

    NoValue en evaluate, se aplica el patron Singleton

--- maven/src/main/java/java/value/NoValue.java
+++ maven/src/main/java/java/value/NoValue.java
@@ -1,19 +1,24 @@
 package java.value;
 
 public class NoValue extends MaybeValue { // Con esta clase seguimos el patron NullObject.
-
-    // HAY QUE APLICARLE EL SINGLETON.
     
+    private static NoValue instance = new NoValue();
+
+    private NoValue(){
+    }
+
     @Override
     public boolean hasValue() {
         // TODO Auto-generated method stub
         return false;
     }
 
+    /**
+     * Se aplica el patron Singleton.
+     */
     @Override
     public MaybeValue evaluate() {
-        // TODO Auto-generated method stub
-        return null;
+        return instance;
     }
     
 }
