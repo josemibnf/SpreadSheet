81a1f49892adfad95cf832fbd7bd1c5746e7ba92
jal04 <josemi.bnf@outlook.es>
2020-05-18 13:56:52 +0200

    SimpleTest falla con get(a1)

--- maven/src/test/java/spreadsheet/SimpleTest.java
+++ maven/src/test/java/spreadsheet/SimpleTest.java
@@ -7,19 +7,28 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import spreadsheet.value.SomeValue;
+
 public class SimpleTest {
 
     @Before
     public void setUpSheet() throws InvalidCell {
         put("c2", 42);
         put("a1", "c2");
-
     }
 
     @Test
     public void cell_has_value_if_it_has_value_c2__before() throws InvalidCell {
         assertTrue(get("c2").hasValue());
     }
+
+    @Test
+    public void cell_has_value_if_evaluate_work_c2__before() throws InvalidCell {
+        SomeValue val = (SomeValue) get("c2").evaluate();
+        System.out.println(val);
+        assertTrue(val.getValue()==42);
+    }
+    
 
     @Test
     public void cell_has_value_if_it_has_value_a1() throws InvalidCell {
