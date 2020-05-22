8f8197241fa2b7880b57bab8eb7fb4f7f3678df3
javigoo <javierroiggregorio@gmail.com>
2020-05-22 14:19:24 +0200

    Set default ComplexSpreadSheetTest

--- test/ComplexSpreadSheetTest.java
+++ test/ComplexSpreadSheetTest.java
@@ -1,15 +1,17 @@
 import SpreadSheet.SpreadSheet;
 import Exceptions.InvalidCell;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 import Value.SomeValue;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ComplexSpreadSheetTest extends SpreadSheet {
 
-    @Before
+    @BeforeEach
     public void setUp() throws InvalidCell {
         put("c1", mult("a1", "b1"));
         put("c2", mult("a2", "b2"));
@@ -20,44 +22,17 @@ public class ComplexSpreadSheetTest extends SpreadSheet {
     }
 
     @Test
-    public void value_of_a1() throws InvalidCell {
-        SomeValue val = (SomeValue) get("a1");
-        assertTrue(val.getValue()==10);
-    }
-
-    @Test
-    public void value_of_c1() throws InvalidCell {
-        SomeValue val = (SomeValue) get("c1");
-        assertTrue(val.getValue()==200);
-    }
-
-    @Test
-    public void value_of_c2() throws InvalidCell {
-        SomeValue val = (SomeValue) get("c2");
-        assertTrue(val.getValue()==1200);
-    }
-
-    @Test
     public void chained_expressions() throws InvalidCell {
-        SomeValue val = (SomeValue) get("c3");
-        assertTrue(val.getValue()==1400);
-    }
-
-    @Test
-    public void chained_propagations_a1() throws InvalidCell {
-        put("a1", "b1");
-        SomeValue val = (SomeValue) get("a1");
-        assertTrue(val.getValue()==20);
+        assertEquals(new SomeValue(1400), get("c3"));
     }
 
     @Test
     public void chained_propagations() throws InvalidCell {
         put("a1", "b1");
-        SomeValue val = (SomeValue) get("c3");
-        assertTrue(val.getValue()==1600);
+        assertEquals(new SomeValue(1600), get("c3"));
     }
 
-    @After
+    @AfterEach
     public void tearDown(){
         clear();
     }
